ab0c77f28065787e4cdca49381bfa151d51175f1
loboprogrammingg <matheusloboo2001@gmail.com>
2024-08-08 15:26:17 -0400

    Iniciando o streamlit

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 
-st.title('Flix App')
-st.divider()
-st.text_input('Nome do Filme:')
-st.button('Salvar')
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-st.line_chart(chart_data)
+def main():
+    st.title('Flix App')
+
+    menu_option = st.sidebar.selectbox(
+        'Selecione Uma opcao',
+        ['Inicio', 'Generos', 'Atores/Atrizes', 'Filmes', 'Avaliacoes']
+    )
+
+    if menu_option == 'Inicio':
+        st.write('Inicio')
+
+    if menu_option == 'Generos':
+        st.write('Lista de Generos')
+
+    if menu_option == 'Atores/Atrizes':
+        st.write('Lista de Atores/Atrizes')
+
+    if menu_option == 'Filmes':
+        st.write('Lista de Filmes')
+
+    if menu_option == 'Avaliacoes':
+        st.write('Lista de Avaliacoes')
+
+if __name__ == '__main__':
+    main()
