85215a835ceaac337e090cc3132ee650c537a702
loboprogrammingg <matheusloboo2001@gmail.com>
2024-08-09 15:22:34 -0400

    Adicionando o Login

---
+++ login/service.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from api.service import Auth
+
+
+def login(username, password):
+    auth_service = Auth()
+    response = auth_service.get_token(
+        username=username,
+        password=password,
+    )
+    if response.get('error'):
+        st.error(f'Falha ao realizar login: {response.get("error")}')
+    else:
+        st.session_state.token = response.get('access')
+        st.rerun()
