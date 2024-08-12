a9eff78741fa03a3a368d80a17a36741129cfc5b
loboprogrammingg <matheusloboo2001@gmail.com>
2024-08-12 15:33:30 -0400

    Adicionando actors na API

--- actors/page.py
+++ actors/page.py
@@ -1,36 +1,49 @@
 import pandas as pd
 import streamlit as st
+from datetime import datetime
 from st_aggrid import AgGrid
-
-
-actors = [
-    {
-        'id': 1,
-        'name': 'Leonardo DiCaprio'
-    },
-    {
-        'id': 2,
-        'name': 'Chris Rock'
-    },
-    {
-        'id': 3,
-        'name': 'Stallone'
-    },
-]
+from actors.service import ActorService
 
 
 def show_actors():
-    st.write('Lista de Atores:')
+    actor_service = ActorService()
+    actors = actor_service.get_actors()
 
-    AgGrid(
-        data=pd.DataFrame(actors),
-        reload_data=True,
-        enableSorting=True,
-        key='actors_grid',
-        height=200,
-    )
+    if actors:
+
+        st.write('Lista de Atores:')
+        actors_df = pd.json_normalize(actors)
+        AgGrid(
+            data=actors_df,
+            reload_data=True,
+            key='actors_grid',
+            height=200,
+        )
+    else:
+        st.warning('Nenhum Ator/Atriz encontrado.')
 
     st.title('Cadastrar Novo Ator/Atriz')
     name = st.text_input('Nome do(a) Ator/Atriz')
+    birthday = st.date_input(
+        label='Data de Nascimento',
+        value=datetime.today(),
+        min_value=datetime(1800, 1, 1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY',
+    )
+    nationality_dropdown = ['BRAZIL', 'USA']
+    nationality = st.selectbox(
+        label='Nacionalidade',
+        options=nationality_dropdown,
+    )
+
     if st.button('Cadastrar'):
-        st.success(f'Ator/Atriz "{name}" Cadastrado com sucesso')
+        new_actor = actor_service.create_actor(
+            name=name,
+            birthday=birthday,
+            nationality=nationality,
+        )
+        if new_actor:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar Ator/Atriz. Verifique os campos.')
